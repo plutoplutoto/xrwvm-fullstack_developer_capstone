2d0dd64210ededcd0a57b801322410cd2ee25e46
Plutoplutoto <nattiyakrajangpit@gmail.com>
2025-05-21 05:30:50 -0400

    added model and admin

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,23 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+# Inline class to show CarModel in CarMake admin page
+class CarModelInline(admin.TabularInline):  # or use StackedInline for vertical layout
+    model = CarModel
+    extra = 1  # Number of empty forms to display
 
-# Register your models here.
+# Custom admin for CarModel (optional, but useful for filtering/searching)
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year', 'dealer_id')
+    list_filter = ('type', 'year')
+    search_fields = ('name', 'car_make__name')
 
-# CarModelInline class
+# Custom admin for CarMake with inline CarModels
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country')
+    search_fields = ('name',)
+    inlines = [CarModelInline]
 
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+# Register models with admin site
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
