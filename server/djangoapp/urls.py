7cda89fa370c8de2841bad587b44916fb8780836
Plutoplutoto <nattiyakrajangpit@gmail.com>
2025-05-20 20:56:59 -0400

    added path in url

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,19 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from django.views.generic import TemplateView
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
+    #    path(route='login', view=views.login_user, name='login'),
+    #    path('login/', TemplateView.as_view(template_name="index.html")),
     # path for dealer reviews view
 
     # path for add a review view
-
+     path('login/', views.login_user, name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
