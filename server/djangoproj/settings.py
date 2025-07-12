9e744a5124aae5b1c5778f4a101acb621a2eece9
Plutoplutoto <nattiyakrajangpit@gmail.com>
2025-07-12 04:40:50 -0400

    linted 13.3

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -108,4 +108,5 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': (
             'django.contrib.auth.password_validation.'
-            '
+            'UserAttributeSimilarityValidator'
+        ),
