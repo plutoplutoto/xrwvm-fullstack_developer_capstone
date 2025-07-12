302b5268611df783db5cf895904eca030f01277a
Plutoplutoto <nattiyakrajangpit@gmail.com>
2025-07-12 04:35:08 -0400

    linted 12

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -122,4 +122,7 @@ def add_review(request):
         return JsonResponse({"status": 200})
     except Exception as e:
         print("Network exception occurred:", str(e))
-        return JsonResponse({"status": 401, "message": "Error in posting review"})
+        return JsonResponse({
+            "status": 401,
+            "message": "Error in posting review"
+        })
