11dde9e730d4136865e5732e33f0ba289a6d35e8
Plutoplutoto <nattiyakrajangpit@gmail.com>
2025-07-12 04:02:34 -0400

    linted 8 model.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -16,12 +16,13 @@ class Dealer(models.Model):
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField
-    country = models.CharField(max_length=100, blank=True) 
+    country = models.CharField(max_length=100, blank=True)
     founded_year = models.PositiveIntegerField(blank=True, null=True)
     # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
+
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
@@ -34,11 +35,12 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+        year = models.IntegerField(default=2023,
+            validators=[
+                MaxValueValidator(2023),
+                MinValueValidator(2015)
+            ]
+    )
     horsepower = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
